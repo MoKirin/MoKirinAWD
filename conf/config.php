4880f05f0c2ebe76efbf9cf70314dd88e1f4d54a
iFurySt <hk326459@163.com>
2017-05-04 16:32:23 +0800

    Configure SQL

---
+++ conf/config.php
@@ -0,0 +1,7 @@
+<?php
+define("DBMS","mysql");
+define('DB_HOST','localhost');
+define('DB_NAME','awd');
+define('DB_USER','root');
+define('DB_PASS','root');
+?>
---
+++ conf/conn.php
@@ -0,0 +1,15 @@
+<?php
+include_once("./config.php");
+$dsn = DBMS.":host=".DB_HOST.";dbname=".DB_NAME;
+try{
+    $pdo = new PDO($dsn,DB_USER,DB_PASS);
+    // echo "OK";
+    // foreach ($pdo->query("SELECT * FROM flag") as $row) {
+    //     print_r($row);
+    //     echo $row["team1"];
+    // }
+    // $pdo = null;
+}catch(PDOException $e){
+    die("[!] Error: ".$e->getMessage()."<br/>");
+}
+?>
